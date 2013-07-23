c8f9ed6d170c5e25a2d5cbc502589f5bd16e37bc
jasuca <service@jasuca.com>
2013-07-24 01:45:01 +0200

    simplify keys input

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -6,39 +6,14 @@ define encmount::mount(
   $mount=$name,
   $ensure='mounted',
   $options='defaults',
-  $temp="/dev/shm/${mapper}"
 ) {
   $devmapper = "/dev/mapper/${mapper}"
 
-  file { $temp:
-    ensure  => 'present',
-    backup  => false,
-    owner   => root,
-    group   => root,
-    mode    => '0400',
-    notify  => Exec["create key $mapper"],
-  }
-
-  exec { "create key $mapper":
-    command     => "/bin/echo -n '${key}' > ${temp}",
-    unless      => "/bin/mount | /bin/grep ${mapper}",
-    refreshonly => true,
-    notify      => Exec["luksOpen $mapper"],
-  }
-
-  exec { "delete key $mapper":
-    command     => "/usr/bin/shred -u ${temp}",
-    refreshonly => true,
-  }
-
   exec { "luksOpen $mapper":
-    command     => "/sbin/cryptsetup --key-file ${temp} luksOpen ${device} ${mapper}",
-    onlyif      => "/usr/bin/test ! -b ${devmapper}",
+    command     => "/bin/echo -n '${key}' | /sbin/cryptsetup luksOpen ${device} ${mapper}",
+    onlyif      => "/sbin/cryptsetup isLuks ${device}",
     creates     => $devmapper,
     path        => ['/sbin', '/bin/'],
-    refreshonly => true,
-    subscribe   => Exec["create key $mapper"],
-    notify      => [ Exec["delete key $mapper"], Mount[$mount] ],
   }
 
   mount { $mount:
